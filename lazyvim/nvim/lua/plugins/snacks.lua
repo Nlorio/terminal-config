5726df4198296695dc7ab1f631e4104c571a042c
nlorio-notion <nlorio@makenotion.com>
2025-12-07 20:41:05 -0800

    Add - lazyvim config

---
+++ lazyvim/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- Enable the terminal feature (required for claudecode.nvim)
+      terminal = { enabled = true },
+    }
+  }
